20f68db4fdad72b0d8d9cb08f9c533645d082081
muditlms <mudit.jain@lmsin.com>
2016-11-29 18:01:59 +0530

    update
    df

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+var cp          = require('child_process');
+var sourcemaps = require('gulp-sourcemaps');
+
+/**
+ * Wait for jekyll-build, then launch the Server
+ */
+
+gulp.task('browser-sync', ['sass'], function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        // port: 8081,
+        notify: false
+    });
+});
+
+/**
+ * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+ */
+gulp.task('sass', function () {
+    return gulp.src('app/assets/css/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: ['scss'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('app/assets/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('app/assets/css'));
+});
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+ //
+ // gulp.task('sync', function () {
+ //     return gulp.src('app/index.html')
+ //         .pipe(browserSync.reload({stream:true}))
+ // });
+
+
+gulp.task('watch', function () {
+    gulp.watch(['app/assets/css/**/*.*','app/*.html'], ['sass']);
+    // gulp.watch(['app/views/*.html', 'app/assets/js/*.js'], ['sync']);
+    // //js files
+    // gulp.watch('app/**/*.js');
+    // svgs
+    // gulp.watch('assets/svg/sprite.svg', ['browser-sync']);
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+*/
+gulp.task('default', ['browser-sync', 'watch']);
